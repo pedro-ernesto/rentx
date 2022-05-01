8ade7ab6841549ee5623dfa787b4448ed2e8d15d
pedro-ernesto <silveira-pedro@outlook.com>
2022-05-01 14:59:31 -0300

    image slider

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useRef, useState} from 'react';
+import { FlatList, ViewToken } from 'react-native';
 
 import { Container,
   ImageIndexes,
@@ -7,28 +7,57 @@ import { Container,
   CarImageWrapper,
   CarImage,
 
-} from './styles';
+} from './styles'; 
 
 interface Props {
   imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+}
+
 export default function ImageSlider({imagesUrl} : Props){
+  const [imageIndex, setImageIndex] = useState<Number>(0);
+
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    // index nunca será nulo, usa exclamação
+    const index = info.viewableItems[0].index!;
+    setImageIndex(index);
+  });
+
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active = {true} />
-        <ImageIndex active = {false} />
-        <ImageIndex active = {false} />
-        <ImageIndex active = {false} />
+        {
+          imagesUrl.map((_, index) => (
+            <ImageIndex
+              key={String(index)}
+              active = {index === imageIndex} 
+            />
+          ))
+          
+        }
       </ImageIndexes>
 
-      <CarImageWrapper>
-        <CarImage
-          source={{uri: imagesUrl[0]}}
-          resizeMode='contain'
+
+        <FlatList 
+          data={imagesUrl}
+          keyExtractor={key => key}
+          renderItem={({item}) => (
+            <CarImageWrapper>
+              <CarImage
+              source={{uri: item}}
+              resizeMode='contain'
+              />
+            </CarImageWrapper>
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          onViewableItemsChanged={indexChanged.current}
         />
-      </CarImageWrapper>
+
     </Container>
   );
 }
